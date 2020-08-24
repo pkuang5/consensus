7a7794533d37ece01dcf2854ce6e7b51f0a150b4
Patrick Kuang <pkuang@ucsb.edu>
2020-08-23 17:53:58 -0700

    pk - search function

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 let yelpREST = axios.create({
     baseURL: `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/`,
     headers: {
-        Authorization: `Bearer nL08itDQ0UA0STOzhtKROkt_RWkdKfqy7mXYDYAbf7_0ZGRxDt08vY90sJ507cNXG3E6KevIW8I_CWrlthX2Wfs9FrQGKWy2YtG0RuxJc7t31-URMsbMZgYVTp1BX3Yx`,
+        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
         "Content-type": "application/json",
     },
 })
