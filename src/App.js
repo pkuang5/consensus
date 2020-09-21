4d07e5b5f0cbfbfa160dce182fd5c907a81da304
Adarsh Garg <gargadarsh10@gmail.com>
2020-09-20 20:14:30 -0700

    resolve

--- src/App.js
+++ src/App.js
@@ -1,85 +1,30 @@
 import React, { useState, useEffect } from "react";
-import database from "./firebase";
-import yelpREST from "./api/yelp";
-import Poll from "./components/poll";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./components/search";
-import JoinCode from "./components/joinCode";
-import CreateCode from "./components/createCode";
 //import Progress from './components/progress'
 import Question from "./components/question";
-import Deck from "./components/Deck";
-import Loader from "./components/loader"
+import Poll from "./components/Poll";
+import Loader from "./components/loader";
+import Progress from "react-progressbar";
 //import Questionnaire from "./components/questionnaire";
 
 function App() {
-  const [businesses, setBusinesses] = useState([]);
-  const [groupCode, setGroupCode] = useState(0);
-  const [cardData, setCardData] = useState([])
-
-  useEffect(() => {
-    if (businesses.length != 0) populateBusinesses(businesses)
-  }, [businesses])
-
-  async function populateBusinesses(idArray) {
-    idArray.reduce(async (memo, id) => {
-      await memo
-      await yelpREST(`/businesses/${id}`).then(({ data }) => {
-        var item = {
-          id: data.id,
-          name: data.name,
-          pics: data.photos
-        }
-        setCardData(cardData => [...cardData, item])
-      })
-    }, undefined)
-  }
-
   return (
-    <React.Fragment>
-      {/* <div>
-        {!firstPress ? (
-          <button
-            onClick={next}
-            class="w-40 h-10 bg-blue-600 text-white center"
-          >
-            Create a Group
-          </button>
-        ) : (
-          <div>
-            {currentQuestion == 0 ? (
-              <Questionnaire
-                message={
-                  questionnaire[answers.length / questions.length].message
-                }
-                onNext={handleContinue}
-              />
-            ) : (
-              <Question
-                question={questions[currentQuestion].question}
-                id={question.id}
-                onContinue={handleContinue}
-                onPrevious={handlePrevious}
-              />
-            )}
-          </div>
-        )}
-      </div> */}
-      {/* <div>
-        <Question />
-      </div> */}
-      {/* <CreateCode
-        businesses={businesses}
-        onCreateCode={(code) => setGroupCode(code)}
-        groupCode={groupCode}
-      /> */}
-      {/* <JoinCode
-        onJoinCode={(code) => setGroupCode(code)}
-        populateBusinesses={(businesses) => setBusinesses(businesses)}
-      /> */}
-      {businesses.length == 0 ? <Search onSubmitSearch={(businesses) => setBusinesses(businesses)} />: null}
-      {cardData.length != businesses.length && businesses.length != 0 ? <div class="h-screen w-screen flex justify-center items-center"><Loader loading={true} /></div>: null}
-    {cardData.length == businesses.length && businesses.length != 0 ? <Deck data={cardData} groupCode={groupCode} />: null}
-    </React.Fragment>
+    <Router>
+      <Switch>
+        <Route path="/" exact strict component={() => <Search />} />
+        <Route
+          path="/:groupCode"
+          exact
+          strict
+          component={({ match }) => (
+            <div>
+              <Poll groupCode={match.params.groupCode} />
+            </div>
+          )}
+        />
+      </Switch>
+    </Router>
   );
 }
 
