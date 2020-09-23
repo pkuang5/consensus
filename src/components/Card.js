090e28ad18f7eaf236e5f2a8236590dc54e91545
Zion Wang <zion@ucsb.umail.edu>
2020-09-23 10:09:33 -0700

    Fixed Bug: the side of each card you flip to is now interactable accordingly using the z-axis

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,19 @@ import yelpREST from "../api/yelp";
 import Map from './Map'
 import 'font-awesome/css/font-awesome.min.css'
 
-
 function Card({ i, x, y, rot, scale, trans, bind, data }) {
   const { lat, lng, name, photos } = data[i];
-
   const [flipped, set] = useState(false)
-  const { transform, opacity } = useSpring({
+  const { transform, opacity} = useSpring({
     opacity: flipped ? 1 : 0,
     transform: `perspective(600px) rotateY(${flipped ? 180 : 0}deg)`,
     config: { mass: 5, tension: 500, friction: 80 }
   })
+
+  // async function focusBack(id) {
+  //   //getElementById('back').style.zIndex = 1;
+  //   id.style.zIndex = -1;
+  // }
 
   return (
     <animated.div
@@ -31,17 +34,17 @@ function Card({ i, x, y, rot, scale, trans, bind, data }) {
         }}
       >
         <div className="card" >
-          <animated.div className="c" style={{ opacity, transform: transform.interpolate(t => `${t} rotateY(180deg)`)} }>
-            <Map center={{ lat: lat, lng: lng }} name={name} zoom={15} />
-            <button class="p-8 bg-blue-600" onClick={() => set(state => !state)}><i class="fa fa-info"></i></button>
-          </animated.div>
-          <animated.div className="c" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>
+          <animated.div className="frontface" id="front" style={{ opacity: opacity.interpolate(o => 1 - o), transform, zIndex: flipped ? -1 : 1 }}>
             <Carousel>
               {photos.map((pic, index) => (
                 <img src={pic} key={index} alt="profilePicture" />
               ))}
             </Carousel>
             <p class="text-center text-xl">{name}</p>
+            <button class="p-8 bg-blue-600" onClick={() => set(state => !state)}><i class="fa fa-info"></i></button>
+          </animated.div>
+          <animated.div className="backface" id="back" style={{ opacity, transform: transform.interpolate(t => `${t} rotateY(180deg)`), zIndex: flipped ? 1 : -1 } }>
+            <Map center={{ lat: lat, lng: lng }} name={name} zoom={15} />
             <button class="p-8 bg-blue-600" onClick={() => set(state => !state)}><i class="fa fa-info"></i></button>
           </animated.div>
         </div>
