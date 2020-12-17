58b1a4685bb087092899634bb66ebb0ae4b0f213
Patrick Kuang <pkuang@ucsb.edu>
2020-12-16 22:59:16 -0800

    responsive height and change color of progress bar

--- src/components/Poll.jsx
+++ src/components/Poll.jsx
@@ -3,6 +3,7 @@ import database from '../firebase'
 import Deck from './Deck'
 import Loader from './loader'
 import Progress from 'react-progressbar'
+import ProgressBar from '@ramonak/react-progress-bar';
 import { use100vh } from 'react-div-100vh'
 
 function Poll(props) {
@@ -27,22 +28,10 @@ function Poll(props) {
                 <p class="text-4xl absolute text-center mt-5 font-bold">consensus</p>
                 <Deck groupCode={props.groupCode} data={data} setProgressPercentage={(percent) => setProgressPercentage(percent)}/>
             </div>
-            <Progress completed={progressPercentage} />
+            {/* <Progress style={{background: 'rgba(255, 255, 255, 0.3)', height:'4rem', color:'#FFE4C4'}} completed={progressPercentage} /> */}
+            <ProgressBar bgcolor='rgba(255, 255, 255, 0.6)' baseBgColor='rgba(255, 255, 255, 0.3)' labelSize='0px' borderRadius='0px' completed={progressPercentage}/>
         </React.Fragment>
     )
-
-  if (data.length == 0) return <Loader loading={true} />;
-  else
-    return (
-      <div>
-        <Progress completed={progressPercentage} />
-        <Deck
-          groupCode={props.groupCode}
-          data={data}
-          setProgressPercentage={(percent) => setProgressPercentage(percent)}
-        />
-      </div>
-    );
 }
 
 export default Poll;
---
+++ src/components/landing.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+function Landing(props) {
+  let history = useHistory();
+
+}
+
+export default Landing;
