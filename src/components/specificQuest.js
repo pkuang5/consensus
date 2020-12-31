013a26a73f975a89c68f04511291af862615aa82
Adarsh Garg <gargadarsh10@gmail.com>
2020-12-30 18:16:27 -0800

    fixed conflicts

--- src/components/specificQuest.js
+++ src/components/specificQuest.js
@@ -8,16 +8,14 @@ import "../styles/Question.css";
 
 function SpecificQuest(){
     //const intro = "Let's get started! Here are a couple questions to help us provide your group with the best options! The next few questions will help us provide your group with the best options! Swipe up to move to the next question!";
-    render() {
-        return (
-            <div className="text-page max-w-md flex mx-auto p-6 mt-10 rounded-lg">
-                <div className="ml-6 pt-1">
-                    <p className="text-question text-base text-black-700 leading-normal text-center">
-                    Let's get started! Here are a couple questions to help us provide your group with the best options! The next few questions will help us provide your group with the best options! Swipe up to move to the next question!
-                    </p>
-                </div>
+    return (
+        <div className="text-page max-w-md flex mx-auto p-6 mt-10 rounded-lg">
+            <div className="ml-6 pt-1">
+                <p className="text-question text-base text-black-700 leading-normal text-center">
+                Let's get started! Here are a couple questions to help us provide your group with the best options! The next few questions will help us provide your group with the best options! Swipe up to move to the next question!
+                </p>
             </div>
-          );
-    }
+        </div>
+        );
 }
 export default SpecificQuest;
