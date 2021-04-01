d9df6e755fa2c3946beb4dbe004038efe297d302
Patrick Kuang <pkuang@ucsb.edu>
2021-03-31 17:17:04 -0700

    api working but formatting is wack

--- client/src/api/yelp.js
+++ client/src/api/yelp.js
@@ -1,11 +1,7 @@
 const axios = require('axios')
 
 let yelpREST = axios.create({
-    baseURL: `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/`,
-    headers: {
-        Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
-        "Content-type": "application/json",
-    },
+    baseURL: 'https://us-central1-pk-consensus.cloudfunctions.net/yelp/yelp',
 })
 
 export default yelpREST
--- server/db.js
+++
@@ -1,6 +0,0 @@
-import firebase from 'firebase'
-import config from './config'
-
-const db = firebase.initializeApp(config.firebaseConfig)
-
-module.exports = db
